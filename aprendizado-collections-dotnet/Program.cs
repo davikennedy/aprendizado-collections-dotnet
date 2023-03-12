d4eada8f6bdda2b99a368f14f7f4dfd657500626
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2023-03-11 22:18:48 -0300

    Lidando com arrays

--- aprendizado-collections-dotnet/Program.cs
+++ aprendizado-collections-dotnet/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Lidando com arrays
+
+string aulaIntro = "Introdução às Coleções";
+string aulaModelando = "Modelando a classe Aula";
+string aulaSets = "Trabalhando com conjuntos";
+
+string[] aulas = new string[3];
+aulas[0] = aulaIntro;
+aulas[1] = aulaModelando;
+aulas[2] = aulaSets;
+
+Imprimir(aulas);
+
+static void Imprimir(string[] aulas)
+{
+    foreach (var aula in aulas)
+    {
+        Console.WriteLine(aula);
+    }
+}
