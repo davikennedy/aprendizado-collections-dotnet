b5a3a787d223057e68c4842df4ca0960eb74dd03
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2023-03-14 23:37:31 -0300

    Organizando operações com arrays

---
+++ Arrays/Arrays.cs
@@ -0,0 +1,41 @@
+﻿// Lidando com arrays
+
+string aulaIntro = "Introdução às Coleções";
+string aulaModelando = "Modelando a classe Aula";
+string aulaSets = "Trabalhando com conjuntos";
+
+string[] aulas = new string[3];
+aulas[0] = aulaIntro;
+aulas[1] = aulaModelando;
+aulas[2] = aulaSets;
+
+Imprimir(aulas);
+
+static void Imprimir(string[] aulas)
+{
+    foreach (var aula in aulas)
+    {
+        Console.WriteLine(aula);
+    }
+}
+
+// Localizando índice de uma string, dentro de um array
+Console.WriteLine($"Aula 'Modelando' está no índice: {Array.IndexOf(aulas, aulaModelando)}");
+
+// Métodos de cópia de arrays
+string[] copia = new string[2];
+Array.Copy(aulas, 1, copia, 0, 2);
+Imprimir(copia);
+
+string[] clone = aulas.Clone() as string[];
+Imprimir(clone);
+
+// Utilizando IndexOf para localizar um elemento no array
+int tamanhoDoArray = 3;
+string[] frutas = new string[tamanhoDoArray];
+
+frutas[0] = "banana";
+frutas[1] = "laranja";
+frutas[2] = "morango";
+
+Console.WriteLine($"Posição de morango no array de frutas: {Array.IndexOf(frutas, "morango")}");
---
+++ Listas/Listas.cs
@@ -0,0 +1,30 @@
+﻿string aulaIntro = "Introdução às Coleções";
+string aulaModelando = "Modelando a classe Aula";
+string aulaSets = "Trabalhando com conjuntos";
+
+/*List<string> aulas = new List<string>
+{
+    aulaIntro,
+    aulaModelando,
+    aulaSets
+};*/
+
+List<string> aulas = new List<string>();
+
+aulas.Add(aulaIntro);
+aulas.Add(aulaModelando);
+aulas.Add(aulaSets);
+
+Imprimir(aulas);
+
+static void Imprimir(List<string> aulas)
+{
+    /*foreach (var aula in aulas)
+    {
+        Console.WriteLine(aula);
+    }*/
+
+    aulas.ForEach(aula => Console.WriteLine(aula));
+    Console.WriteLine($"A primeira aula é: {aulas.First()}");
+    Console.WriteLine($"A última aula é: {aulas.Last()}");
+}
