0f9f31179324a08275e0df76fe2ae080279c6543
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2023-03-16 23:32:47 -0300

    Trabalhando com listas de objetos

---
+++ ListasDeObjetos/Aula.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListasDeObjetos
+{
+    public class Aula : IComparable
+    {
+        public Aula(string titulo, int tempo)
+        {
+            Titulo = titulo;
+            Tempo = tempo;
+        }
+
+        public string Titulo { get; set; }        
+        public int Tempo { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            Aula other = obj as Aula;
+            return Titulo.CompareTo(other.Titulo);
+        }
+
+        public override string ToString()
+        {
+            return $"[Título: {Titulo}, Tempo: {Tempo} minutos]";
+        }
+    }
+}
---
+++ ListasDeObjetos/Program.cs
@@ -0,0 +1,28 @@
+﻿using ListasDeObjetos;
+
+var aulaIntro = new Aula("3 - Introdução às Coleções", 20);
+var aulaModelando = new Aula("5 - Modelando a classe Aula", 18);
+var aulaSets = new Aula("2 - Trabalhando com conjuntos", 16);
+
+List<Aula> aulas = new()
+{
+    aulaIntro,
+    aulaModelando,
+    aulaSets
+};
+
+Imprimir(aulas);
+
+aulas.Sort();
+Imprimir(aulas);
+
+aulas.Sort((este, outro) => este.Tempo.CompareTo(outro.Tempo));
+Imprimir(aulas);
+
+static void Imprimir(List<Aula> aulas)
+{
+    Console.Clear();
+
+    foreach(var aula in aulas) 
+        Console.WriteLine(aula);
+}
