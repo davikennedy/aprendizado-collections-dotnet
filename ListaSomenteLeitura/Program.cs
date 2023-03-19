74da2829a01831e8debb398308b5a69cf06c23e6
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2023-03-18 21:14:17 -0300

    Realizando ordenação de Aulas

--- ListaSomenteLeitura/Program.cs
+++ ListaSomenteLeitura/Program.cs
@@ -1,14 +1,22 @@
 ﻿using ListaSomenteLeitura;
 
 Curso cSharpColecoes = new Curso("C# Collections", "Marcelo");
-cSharpColecoes.Adicionar(new Aula("Trabalhando com Listas", 21));
 
+cSharpColecoes.Adicionar(new Aula("Trabalhando com Listas", 21));
 Imprimir(cSharpColecoes.Aulas);
+
+cSharpColecoes.Adicionar(new Aula("Criando uma Aula", 20));
+cSharpColecoes.Adicionar(new Aula("Modelando com Coleções", 19));
+Imprimir(cSharpColecoes.Aulas);
+
+// Ordenando aulas
+var aulasCopiadas = new List<Aula>(cSharpColecoes.Aulas);
+aulasCopiadas.Sort();
+Imprimir(aulasCopiadas);
 
 static void Imprimir(IList<Aula> aulas)
 {
     Console.Clear();
-    //aulas.ForEach(aula => Console.WriteLine(aula));
 
     foreach(var aula in aulas) 
         Console.WriteLine(aula);
