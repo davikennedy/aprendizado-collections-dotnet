9483228eaabfe62d7d6923b020c8d5795ac3dea8
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2023-03-19 22:46:31 -0300

    Trabalhando com Sets

---
+++ Conjuntos/Program.cs
@@ -0,0 +1,11 @@
+﻿/* Propriedades dos Sets:
+ * Sets não permitem duplicidade
+ * Os elementos não são mantidos em ordem específica
+ */
+
+ISet<string> alunos = new HashSet<string>();
+alunos.Add("Vanessa Tonini");
+alunos.Add("Ana Losnak");
+alunos.Add("Rafael Nercessian");
+
+Console.WriteLine(String.Join(", ", alunos));
