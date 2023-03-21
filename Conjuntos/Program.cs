14d8dba7b73190882ce6e615eb18e8af47f14ed5
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2023-03-20 23:01:37 -0300

    Realizando ordenação de Sets

--- Conjuntos/Program.cs
+++ Conjuntos/Program.cs
@@ -7,5 +7,11 @@ ISet<string> alunos = new HashSet<string>();
 alunos.Add("Vanessa Tonini");
 alunos.Add("Ana Losnak");
 alunos.Add("Rafael Nercessian");
-
+alunos.Add("Marcos Aurélio");
+alunos.Add("Paulo Júnior");
+alunos.Add("João Gomes");
 Console.WriteLine(String.Join(", ", alunos));
+
+var alunosEmLista = new List<string>(alunos);
+alunosEmLista.Sort();
+Console.WriteLine(String.Join(", ", alunosEmLista));
