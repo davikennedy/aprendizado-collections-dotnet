0c44aa97607050e648a4c4d14eaec1b16a5081f6
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2023-04-01 23:55:59 -0300

    Estruturando projeto de ListasClassificadas

---
+++ ListasClassificadas/Aluno.cs
@@ -0,0 +1,29 @@
+﻿namespace Conjuntos;
+
+public class Aluno
+{
+    public string Nome { get; set; }
+    public int Matricula { get; set; }
+
+    public Aluno(string nome, int numeroMatricula)
+    {
+        Nome = nome;
+        Matricula = numeroMatricula;
+    }
+
+    public override string ToString()
+    {
+        return $"Nome: {Nome}\nMatrícula: {Matricula}\n";
+    }
+
+    public override bool Equals(object? obj)
+    {
+        Aluno outro = obj as Aluno;
+        return Nome.Equals(outro.Nome);
+    }
+
+    public override int GetHashCode()
+    {
+        return Nome.GetHashCode();
+    }
+}
