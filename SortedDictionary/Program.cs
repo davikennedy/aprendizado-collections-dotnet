b12e24d46aa7afab79784a980bdee7d47d9dc4e1
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2023-04-03 22:52:22 -0300

    Aprendendo a utilizar SortedDictionary

---
+++ SortedDictionary/Program.cs
@@ -0,0 +1,15 @@
+﻿using SortedDictionary;
+
+IDictionary<string, Aluno> sorted = new SortedDictionary<string, Aluno>();
+
+sorted.Add("WM", new Aluno("Wanderson", 11287));
+sorted.Add("VT", new Aluno("Vanessa", 34672));
+sorted.Add("AL", new Aluno("Ana", 56171));
+sorted.Add("RN", new Aluno("Rafael", 143763));
+
+Console.WriteLine();
+
+foreach (var item in sorted)
+{
+    Console.WriteLine(item);
+}
