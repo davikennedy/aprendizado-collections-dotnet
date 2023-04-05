ba7ed2c24b6abea867a31833a9fd2864dacc9c9e
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2023-04-04 21:49:51 -0300

    Aprendendo a utilizar SortedSet

---
+++ SortedSet/Program.cs
@@ -0,0 +1,24 @@
+﻿ISet<string> alunos = new SortedSet<string>(new CompararNomesIguais())
+{
+    "Vanessa Tonini",
+    "Ana Losnak",
+    "Rafael Nercessian",
+    "Priscila Stuani"
+};
+
+alunos.Add("Rafael Rollo");
+alunos.Add("Fabio Gushiken");
+alunos.Add("Fabio Gushiken");
+
+foreach (var aluno in alunos)
+{
+    Console.WriteLine(aluno);
+}
+
+internal class CompararNomesIguais : IComparer<string>
+{
+    public int Compare(string? x, string? y)
+    {
+        return string.Compare(x, y, StringComparison.CurrentCultureIgnoreCase);
+    }
+}
