fc25065b25277c6a6af207084c1df406e096ba64
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2023-04-06 16:48:27 -0300

    Aprendendo a utilizar arrays multidimensionais

---
+++ ArraysMultidimensionais/Program.cs
@@ -0,0 +1,25 @@
+﻿string[,] resultados = new string[4, 3]
+{
+    { "Alemanha", "Espanha", "Itália" },
+    { "Argentina", "Holanda", "França" },
+    { "Holanda", "Alemanha", "Alemanha" },
+    { "Brasil", "Uruguai", "Portugal" }
+};
+
+for (int copa = 0; copa <= resultados.GetUpperBound(1); copa++)
+{
+    int ano = 2014 - copa * 4;
+    Console.Write(ano.ToString().PadRight(12));
+}
+
+Console.WriteLine();
+
+for (int posicao = 0; posicao <= resultados.GetUpperBound(0); posicao++)
+{
+    for (int copa = 0; copa <= resultados.GetUpperBound(1); copa++)
+    {
+        Console.Write(resultados[posicao, copa].PadRight(12));
+    }
+
+    Console.WriteLine();
+}
