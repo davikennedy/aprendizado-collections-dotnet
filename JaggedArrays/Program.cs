67b6913d203b19442a7058a6df57f0aa064fe03d
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2023-04-07 21:30:03 -0300

    Aprendendo a utilizar Jagged Arrays

---
+++ JaggedArrays/Program.cs
@@ -0,0 +1,10 @@
+﻿string[][] familias = new string[3][];
+
+familias[0] = new string[] { "Fred", "Wilma", "Pedrita" };
+familias[1] = new string[] { "Homer", "Marge", "Lisa", "Bart", "Maggie" };
+familias[2] = new string[] { "Florinda", "Kiko" };
+
+foreach (var familia in familias)
+{
+    Console.WriteLine(string.Join(", ", familia));
+}
