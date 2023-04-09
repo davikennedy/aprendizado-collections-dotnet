d5215124f51e9ba10161c4d6903ad933e627a0d4
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2023-04-08 23:42:32 -0300

    Realizando consultas em Collections

---
+++ ConsultandoCollections/Program.cs
@@ -0,0 +1,45 @@
+﻿List<Mes> meses = new List<Mes>()
+{
+   new Mes("Janeiro     ", 31),
+   new Mes("Fevereiro   ", 28),
+   new Mes("Março       ", 31),
+   new Mes("Abril       ", 30),
+   new Mes("Maio        ", 31),
+   new Mes("Junho       ", 30),
+   new Mes("Julho       ", 31),
+   new Mes("Agosto      ", 31),
+   new Mes("Setembro    ", 30),
+   new Mes("Outubro     ", 31),
+   new Mes("Novembro    ", 30),
+   new Mes("Dezembro    ", 31)
+};
+
+meses.Sort();
+
+foreach (var mes in meses)
+{
+    if (mes.Dias != 31) continue; 
+    Console.WriteLine(mes.Nome.ToUpper());
+}
+public class Mes : IComparable
+{
+    public string Nome { get; private set; }
+    public int Dias { get; private set; }
+
+    public Mes(string nome, int dias)
+    {
+        Nome = nome;
+        Dias = dias;
+    }
+
+    public override string ToString()
+    {
+        return $"{Nome} - {Dias}";
+    }
+    
+    public int CompareTo(object? obj)
+    {
+        Mes? outro = obj as Mes;
+        return Nome.CompareTo(outro.Nome);
+    }
+}
