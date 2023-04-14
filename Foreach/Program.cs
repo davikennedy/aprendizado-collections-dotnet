d9dc155739a9cde943c55c7a4259c231aed094ef
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2023-04-13 23:08:47 -0300

    Entendendo o funcionamento do laço Foreach

---
+++ Foreach/Program.cs
@@ -0,0 +1,20 @@
+﻿var meses = new List<string>
+{
+    "Janeiro",
+    "Fevereiro",
+    "Março",
+    "Abril",
+    "Maio",
+    "Junho",
+    "Julho",
+    "Agosto",
+    "Setembro",
+    "Outubro",
+    "Novembro",
+    "Dezembro"
+};
+
+foreach (var mes in meses)
+{
+    Console.WriteLine(mes);
+}
