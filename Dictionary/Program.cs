c2601fd820c1f78faa959c4a0b7ff982a9a5e982
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2023-05-16 22:00:52 -0300

    Entendendo o conceito de Dictionary

---
+++ Dictionary/Program.cs
@@ -0,0 +1,13 @@
+﻿Dictionary<string, string> estados = new Dictionary<string, string>();
+
+estados.Add("SP", "São Paulo");
+estados.Add("BA", "Bahia");
+estados.Add("MG", "Minas Gerais");
+estados.Add("GO", "Goiás");
+
+foreach (var estado in estados)
+{
+    Console.WriteLine($"Chave: {estado.Key}, Valor: {estado.Value}");
+}
+
+estados.Add("BA2", "Bahia");
